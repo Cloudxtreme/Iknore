5ca7091bac1c64a275de1390b30bacbcaa1ac5d8
Yami Odymel <yamiodymel@gmail.com>
2016-02-10 07:38:18 +0000

    Changing the configs.

--- src/master/iknore/configs.php
+++ src/master/iknore/configs.php
@@ -1,27 +1,29 @@
 <?php
-$nodes = ['akaria' => [
-                        'ipv4'      => '127.0.0.1',
-                        'ipv6'      => '::1',
-                        'url'       => 'akaria.iknore.com/',
-                        'bucket'    => 'test',
-                        'accessKey' => 'laravel689',
-                        'secretKey' => '12345678',
-                      ],
-          'baka'   => [
-                        'ipv4'      => '127.0.0.1',
-                        'ipv6'      => '::1',
-                        'url'       => 'baka.iknore.com/',
-                        'bucket'    => 'test1',
-                        'accessKey' => 'laravel689',
-                        'secretKey' => '12345678',
-                      ],
-          'cave'   => [
-                        'ipv4'      => '127.0.0.1',
-                        'ipv6'      => '::1',
-                        'url'       => 'cave.iknore.com/',
-                        'bucket'    => 'test1',
-                        'accessKey' => 'laravel689',
-                        'secretKey' => '12345678',
-                      ],
-         ];
+define('NODES', ['akaria' => [
+                                'ipv4'      => '127.0.0.1',
+                                'ipv6'      => '::1',
+                                'url'       => 'akaria.iknore.com/',
+                                'bucket'    => 'test',
+                                'accessKey' => 'laravel689',
+                                'secretKey' => '12345678',
+                              ],
+                  'baka'   => [
+                                'ipv4'      => '127.0.0.1',
+                                'ipv6'      => '::1',
+                                'url'       => 'baka.iknore.com/',
+                                'bucket'    => 'test1',
+                                'accessKey' => 'laravel689',
+                                'secretKey' => '12345678',
+                              ],
+                  'cave'   => [
+                                'ipv4'      => '127.0.0.1',
+                                'ipv6'      => '::1',
+                                'url'       => 'cave.iknore.com/',
+                                'bucket'    => 'test1',
+                                'accessKey' => 'laravel689',
+                                'secretKey' => '12345678',
+                              ],
+                 ]);
+
+define('SECRET_KEY', '12345678');
 ?>
